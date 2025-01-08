31729eb216e6bfab7ed6d13436a79f0db93eeeca
airida <airida.grasyte0@gmail.com>
2025-01-08 09:00:47 +0200

    Trečia užduotis

---
+++ 3/3.1/03_03.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class LargerThanOrEqualTo {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.parseInt(scan.nextLine());
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.parseInt(scan.nextLine());
+        if (firstNumber > secondNumber) {
+            System.out.println("Grater number is: " + firstNumber);
+        } else if (firstNumber < secondNumber) {
+            System.out.println("Grater number is: " + secondNumber);
+        } else {
+            System.out.println("The numbers are equal!");
+        }
+
+    }
+}
